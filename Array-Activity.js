2bb8f99ffeca3d4565c26dbf7e5add226ada4563
pauuulaaa <heleinapaula@gmail.com>
2024-10-20 20:16:23 +0800

    Create Array-Activity.js

---
+++ Array-Activity.js
@@ -0,0 +1,8 @@
++ // Favorite Singers
++ let favSingers = ["Sabrina Carpenter, Ariana Grande"];
++ console.log("My first favorite singer is", favSingers[0]);
++
++ // Favorite Numbers
++ let favNumbers = ["21", "18", "06", "13", "10"];
++ let mixedArr = ["string", ["otherarray"], 123, true];
++ console.log(mixedArr);
